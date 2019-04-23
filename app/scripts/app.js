c65565c6c0f7e55e97c7dd0b2f8ccf9da9f6d960
andressales <andredki@hotmail.com>
2019-04-23 00:02:25 -0300

    layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,7 +14,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'easypiechart'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,19 @@ angular.module('fastshopApp')
     'themoviedbService', 
     function ($scope, themoviedbService) {
       $scope.movies = [];
+      $scope.percent = function percent(rate){ return parseInt(rate * 10); };
+      $scope.options = {
+        animate:{
+            duration:3000,
+            enabled:true
+        },
+        barColor:'#21d07a',
+        trackColor: '#081c22',
+        scaleColor:false,
+        lineWidth:3,
+        size:22,
+        lineCap:'circle'
+    };
       themoviedbService.listMovie().get()
       .$promise.then(
         function(response){
