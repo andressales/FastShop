103a1134f09d5ce57a1b2a8c70a310dad61fa1f2
ANDRE SOUZA DE SALES <andre.sales@dominio.anhembi.br>
2019-04-23 18:57:09 -0300

    include the app.js to call node

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+ '/app'));
+
+
+app.get('/*', function(req,res){
+    console.log(req);
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname+'/app/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
